206cff28125b39447c20ca2bf2970911801ca9ed
mohamad-ridwan <mr643062@gmail.com>
2025-03-03 23:49:51 +0700

    first commit

---
+++ src/features/employee-form.js
@@ -0,0 +1,126 @@
+(() => {
+  const API_URL = 'http://localhost:3000/employees';
+
+  /**
+   * TODO:
+   * - Add a function to generate random string (10 digits alphanumeric) for id
+   * - Add a function to add new data (submit)
+   * - Add a function to redirect to the index page
+   */
+
+  let employeeForm = {
+    id: '',
+    name: '',
+    position: ''
+  }
+
+  // created variable here for reuse
+  const errResponseMsg = 'A server error has occurred. Please try again.'
+  const successSubmitResponse = 'Successfully added employees'
+
+  const form = document.querySelector('#employeeForm')
+
+  // generate random id for id
+  function generateRandomId(length = 10) {
+    const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789'
+    let randomId = ''
+
+    for (let i = 0; i < length; i++) {
+      const randomIndex = Math.floor(Math.random() * characters.length)
+      randomId += characters[randomIndex];
+    }
+
+    return randomId
+  }
+
+  const randomId = generateRandomId()
+  // update random id to id form data
+  employeeForm = {
+    ...employeeForm,
+    id: randomId
+  }
+  // put random id to id input
+  document.querySelector('input[name="id"]').value = randomId
+
+  // add new data
+  const addNewData = async (formData) => {
+    try {
+      const response = await fetch(API_URL, {
+        method: 'POST',
+        headers: {
+          "Content-Type": "application/json"
+        },
+        body: JSON.stringify(formData)
+      })
+      if (!response.ok) {
+        return errResponseMsg
+      }
+      const result = await response.json()
+      return result
+    } catch (error) {
+      return errResponseMsg
+    }
+  }
+
+  const validateForm = () => {
+    let err = {}
+    if (!employeeForm.name.trim()) {
+      err.name = 'Must be required!'
+    }
+    if (!employeeForm.position.trim()) {
+      err.position = 'Must be required!'
+    }
+
+    return err
+  }
+
+  const handleSubmitAddEmployeeData = async () => {
+    // validate submit form
+    if (Object.keys(validateForm()).length > 0) {
+      alert('Please complete the form!')
+      return
+    }
+
+    const result = await addNewData(employeeForm)
+    if (!result?.id) {
+      alert(errResponseMsg)
+      return
+    }
+    alert(successSubmitResponse)
+  }
+
+  const submitAddEmployeeData = () => {
+    form.addEventListener('submit', (event) => {
+      event.preventDefault()
+      event.stopPropagation()
+
+      handleSubmitAddEmployeeData()
+    })
+  }
+
+  const handleChangeInput = (name) => {
+    document.querySelector(`input[name=${name}]`)?.addEventListener('input', (event) => {
+      employeeForm = {
+        ...employeeForm,
+        [event.target.name]: event.target.value
+      }
+    })
+  }
+
+  document.querySelector('#submitForm').addEventListener('click', (event) => {
+    event.preventDefault()
+    event.stopPropagation()
+
+    handleSubmitAddEmployeeData()
+  })
+
+  document.querySelector('#backToEmployee').addEventListener('click', () => {
+    window.location.href = '../employee/index.html'
+  })
+
+  // call each input
+  Object.keys(employeeForm).forEach((value, index, arr) => {
+    handleChangeInput(value)
+  })
+  submitAddEmployeeData()
+})();
---
+++ src/features/employee.js
@@ -0,0 +1,146 @@
+(async () => {
+  const API_URL = 'http://localhost:3000/employees';
+
+  /**
+   * TODO:
+   * - Add a function to redirect to the form page
+   * - Add a function to fetch data
+   * - Add a function to filter data by employee's name
+   * - Add a function to display table
+   * - Add a function to delete data (bonus)
+   */
+
+  const tableHeaderEmployees = [
+    {
+      name: 'No.'
+    },
+    {
+      name: 'Employee ID'
+    },
+    {
+      name: 'Name'
+    },
+    {
+      name: 'Position'
+    },
+    {
+      name: 'Action'
+    }
+  ]
+
+  // created variable here for reuse
+  const errResponseMsg = 'A server error has occurred. Please try again.'
+  const employessTableEl = document.querySelector('#table-wrapper')
+  const employeeDataEmptyMsg = 'No employee data available'
+  const inputEmployeesEl = document.querySelector('#input-employees-filter')
+
+  // redirect to form page
+  const handleRedirect = (url) => {
+    document.querySelector('#addEmployees')?.addEventListener('click', () => {
+      window.location.href = url // url is for dynamic handle navigate
+    })
+  }
+
+  // get employees data
+  const getEmployeesData = async () => {
+    try {
+      const response = await fetch(API_URL)
+      if (!response.ok) {
+        return errResponseMsg
+      }
+      const result = await response.json()
+      if (result?.length === undefined) {
+        return errResponseMsg
+      }
+      return result
+    } catch (error) {
+      return errResponseMsg
+    }
+  }
+  const employeesData = await getEmployeesData()
+  const employeesFilterData = await getEmployeesData()
+
+  // created display table for reuse it
+  // display employees table
+  const displayEmployeeTable = (employeesData) => {
+    // validate employee data to empty data
+    if (employeesData?.length === undefined || employeesData.length === 0) {
+      employessTableEl.innerHTML = `
+        <p>${employeeDataEmptyMsg}</p>
+      `
+      return
+    }
+
+    employessTableEl.innerHTML = `
+      <table id="employess-data">
+        <tr>
+          ${tableHeaderEmployees.map(head => {
+      return `<th>${head.name}</th>`
+    }).join('')}
+        </tr>
+        ${employeesData.map((employee, index) => {
+      return `
+            <tr>
+              <td>${index + 1}.</td>
+              <td>${employee.id}</td>
+              <td style="min-width: 100px;">${employee.name}</td>
+              <td>${employee.position}</td>
+              <td><button name="delete-${employee.id}" class="btn" style="background-color: #ff0000 !important;">
+                <span class="btn-text">Delete</span>
+              </button></td>
+            </tr>
+          `
+    }).join('')}
+      </table>
+    `
+  }
+
+  // filter employees table feature
+  const searchEmployee = () => {
+    // validate employee data if empty
+    if (employeesData?.length === undefined || employeesData.length === 0) {
+      return
+    }
+
+    inputEmployeesEl.addEventListener('input', (event) => {
+      const filterResult = employeesData.filter(employee =>
+        employee.name?.toLowerCase().includes(event.target.value.toLowerCase())
+      )
+      displayEmployeeTable(filterResult)
+    })
+  }
+
+  const handleDeleteEmployee = async (id) => {
+    try {
+      const response = await fetch(`${API_URL}/${id}`, {
+        method: 'DELETE',
+      })
+      if (!response.ok) {
+        return errResponseMsg
+      }
+      const result = await response.json()
+      if (result?.length === undefined) {
+        return errResponseMsg
+      }
+      return result
+    } catch (error) {
+      return errResponseMsg
+    }
+  }
+
+  // call the function available here
+  // for the first load call the employessData
+  displayEmployeeTable(employeesData)
+  searchEmployee()
+  handleRedirect('../employee-form/index.html')
+  employeesData?.forEach(employee => {
+    document.querySelector(`button[name="delete-${employee.id}"]`)?.addEventListener('click', (e) => {
+      const name = e.target.name
+      if (name) {
+        const currentId = name.split('-')[1]
+        handleDeleteEmployee(currentId)
+        window.location.reload()
+      }
+    })
+  })
+})();
